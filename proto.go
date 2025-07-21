8364a466d3f1623d6334edd84832ca9d703130c3
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-07-21 16:38:53 +0100

    tfjrt 7ala

--- proto.go
+++ proto.go
@@ -4,13 +4,17 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 
 	"github.com/tidwall/resp"
 )
 
 type Command interface{}
-
-type Setcommand struct {
+const (
+	SetCommand = "SET"
+	GetCommand = "GET"
+)
+type SetCommand struct {
 	Key   string
 	Value string
 }
@@ -22,8 +26,28 @@ func parseCommand(raw string) (Command, error) {
 		if err ==io.EOF{
 			break
 		}
+		if err != nil {
+			log.Fatal("Error reading value:", err)
+		}
 		fmt.Println("9rina ", v, err)
-		if v.Type() == resp.TypeArray {
-			fmt.Printf("Received array command: %s\n", v)
+		if v.Type() == resp.Array {
+			for _ ,value := v.Array() {
+				switch value.String() {
+					case SetCommand:
+						fmt.Println("Set command detected",len(value.Array()))
+						if len(v.Array()) != 3 {
+							return nil, fmt.Errorf("invalid SET command format")
+						}
+						cmd:=SetCommand{
+							Key:   v.Array()[1].String(),
+							Value: v.Array()[2].String(),
+						}
+						return cmd, nil
 	}
+}
+
+}
+	}
+	return  "foo bar", nil
+
 }
