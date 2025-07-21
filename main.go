dfb21786d17ce43f70dbd992e4a2e5533b694826
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-07-21 02:24:58 +0100

    start with basic server

--- main.go
+++ main.go
@@ -1,3 +1,36 @@
 package main
 
+import "net"
 
+
+type Config struct{
+	Port int 
+}
+
+type Server struct{
+	Config Config
+	ln net.Listener
+}
+func NewConfig(port int)Config{
+	return  Config{
+		Port: port,
+	}
+}
+
+func NewServer(config Config ) *Server{
+	return &Server{
+		Config: config,
+	}
+}
+func (s *Server) Start() error {
+	ln,err:=net.Listen("tcp", ":"+string(s.Config.Port))
+	if err !=nil {
+		return  err
+	}
+	s.ln= ln
+	return nil
+	
+}
+func (s *Server) Runloop(
+	hey,err:= 
+)
